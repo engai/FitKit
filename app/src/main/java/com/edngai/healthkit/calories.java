b6dd6e3f289a23541661236dac30ce4b80f9886f
Ed Ngai <engai@ucsd.edu>
2015-11-03 23:23:48 -0800

    Querys from parse and displays

--- app/src/main/java/com/edngai/healthkit/calories.java
+++ app/src/main/java/com/edngai/healthkit/calories.java
@@ -6,13 +6,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-<<<<<<< HEAD
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-=======
 import android.widget.Toast;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -25,12 +24,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.parse.FindCallback;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 //Crystal: I commented out some stuff just in case things go wrong
 //I know what the original code looked like; we can clean up later!
->>>>>>> 52e1ea53189914cf53aef6a201de713605b9057d
+
 
 public class calories extends AppCompatActivity {
 
@@ -42,13 +46,11 @@ public class calories extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-<<<<<<< HEAD
         String[] days = {"Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, days);
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
 
-=======
     }*/
 
     private Toolbar toolbar;                              // Declaring the Toolbar Object
@@ -67,6 +69,36 @@ public class calories extends AppCompatActivity {
         setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
         getSupportActionBar().setTitle("Calories");
         getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+
+        final List<String> caloriesArray = new ArrayList<String>();
+        final List<String> amountArray = new ArrayList<String>();
+        ParseQuery query = new ParseQuery("Calories");
+
+        query.orderByDescending("createdAt");
+        query.setLimit(7);
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+
+            @Override
+            public void done(List<ParseObject> caloriesList, ParseException e) {
+
+                if (e == null) {
+
+
+                    for (ParseObject foodItem : caloriesList) {
+                        caloriesArray.add((String) foodItem.get("food"));
+                        amountArray.add((String) foodItem.get("calories"));
+                    }
+
+                    displayToList(caloriesArray, amountArray);
+
+                } else {
+                    //Log.d("Error with calorie retrieval", "Error: " + e.getMessage());
+                }
+            }
+        });
+
 
     }
 
@@ -109,7 +141,7 @@ public class calories extends AppCompatActivity {
         }
 
         return true;
->>>>>>> 52e1ea53189914cf53aef6a201de713605b9057d
+
     }
 
     private TextView resultText;
@@ -172,5 +204,17 @@ public class calories extends AppCompatActivity {
         // create an alert dialog
         AlertDialog alert = alertDialogBuilder.create();
         alert.show();
+    }
+
+    public void displayToList(List<String> caloriesArray, List<String> amountArray){
+        String[] finalCaloriesArray = new String[caloriesArray.size()];
+        caloriesArray.toArray(finalCaloriesArray);
+
+        String[] finalAmountArray = new String[amountArray.size()];
+        amountArray.toArray(finalAmountArray);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(calories.this, android.R.layout.simple_list_item_1, finalCaloriesArray);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
     }
 }
