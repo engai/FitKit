07d1281dea68881336a0c8b84a03f4673d5b9dc2
Ed Ngai <engai@ucsd.edu>
2015-11-03 16:07:17 -0800

    Added calorie table

--- app/src/main/java/com/edngai/healthkit/calories.java
+++ app/src/main/java/com/edngai/healthkit/calories.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class calories extends AppCompatActivity {
 
@@ -15,6 +17,11 @@ public class calories extends AppCompatActivity {
         setContentView(R.layout.activity_calories);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String[] days = {"Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, days);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
 
     }
 
