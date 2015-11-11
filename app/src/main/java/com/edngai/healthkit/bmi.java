446639df9765e90732d1557b6e5766a1aa0e1c2d
Vilan Le <v3le@ucsd.edu>
2015-11-11 00:37:56 -0800

    user checks and additional bmi meaning

--- app/src/main/java/com/edngai/healthkit/bmi.java
+++ app/src/main/java/com/edngai/healthkit/bmi.java
@@ -1,32 +1,26 @@
 package com.edngai.healthkit;
 
-import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.parse.GetCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 public class bmi extends AppCompatActivity {
 
-    private TextView resultBMI;
+    private TextView resultBMI, meaning;
     private EditText weightIn, heightIn;
     private double result;
     private String resultString;
+    private String resultMeaning;
     dataHolder g = dataHolder.getInstance();
 
 
@@ -36,6 +30,7 @@ public class bmi extends AppCompatActivity {
         setContentView(R.layout.activity_bmi);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Your Current BMI");
         initializeApp();
     }
 
@@ -43,6 +38,7 @@ public class bmi extends AppCompatActivity {
         weightIn = (EditText) findViewById(R.id.userWeight);
         heightIn = (EditText) findViewById(R.id.userHeight);
         resultBMI = (TextView) findViewById(R.id.resultOut);
+        meaning = (TextView) findViewById(R.id.description);
 
         // get the latest created object's bmi
 
@@ -69,6 +65,10 @@ public class bmi extends AppCompatActivity {
     public void displayBMI(){
         // display the text
         resultBMI.setText(resultString, TextView.BufferType.NORMAL);
+
+        resultMeaning = getMeaning();
+        // display the bmi meaning
+        meaning.setText(resultMeaning, TextView.BufferType.NORMAL);
     }
 
     /*
@@ -107,39 +107,62 @@ public class bmi extends AppCompatActivity {
     // called when onClick() is called on the Calculate BMI button
     public void runBMI(View v){
 
-        // local variables to hold the weight and height as strings
-        double weight = Double.parseDouble(weightIn.getText().toString());
-        double height = Double.parseDouble( heightIn.getText().toString() );
+        // for user checks
+        AlertDialog.Builder builder1;
+        builder1 = new AlertDialog.Builder(this);
+        // check if the user has inputted a new weight and height
+        if ( (weightIn.getText().length() == 0) || (heightIn.getText().length() == 0) ) {
+            // prompt user to input a weight and height
+            builder1.setMessage("Please enter in both a new weight and height to change your user information.");
 
-        // find the result and display that result bmi
-        result = calculateBMI(weight, height);
-        resultString = String.format("%.2f", result);
-        displayBMI();
-        confirmBMI();
+            builder1.setCancelable(true);
+            builder1.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
 
+            // create and show the alert box
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
+
+        // check for crazy user input (weight is over 3 digits long, height is over 3 digits long)
+        else if ( (weightIn.getText().length() > 3 ) || (heightIn.getText().length() > 3) ){
+            builder1.setMessage("Are you sure that is the correct weight/height?");
+
+            builder1.setCancelable(true);
+            builder1.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+
+            // create and show the alert box
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
+
+        // user has inputted data
+        else {
+            // local variables to hold the weight and height as strings
+            double weight = Double.parseDouble(weightIn.getText().toString());
+            double height = Double.parseDouble(heightIn.getText().toString());
+
+            // find the result and display that result bmi
+            result = calculateBMI(weight, height);
+            resultString = String.format("%.2f", result);
+            displayBMI();
+            confirmBMI();
+        }
     }
 
     // A Pop Up Box Opens indicating what you bmi means
     public void confirmBMI(){
         AlertDialog.Builder builder1;
+        String mes = getMeaning();
         builder1 = new AlertDialog.Builder(this);
-
-        if ( g.getResultInput() < 15 ) {
-            builder1.setMessage("Your BMI means: very severely underweight"); }
-        else if ( (g.getResultInput() >= 15 ) && (g.getResultInput() < 16)  ) {
-            builder1.setMessage("Your BMI means: severely underweight"); }
-        else if ( (g.getResultInput() >= 16 ) && (g.getResultInput() < 18.5)  ) {
-            builder1.setMessage("Your BMI means: underweight"); }
-        else if ( (g.getResultInput() >= 18.5 ) && (g.getResultInput() < 25)  ) {
-            builder1.setMessage("Your BMI means: normal"); }
-        else if ( (g.getResultInput() >= 25 ) && (g.getResultInput() < 30)  ) {
-            builder1.setMessage("Your BMI means: overweight"); }
-        else if ( (g.getResultInput() >= 30 ) && (g.getResultInput() < 35)  ) {
-            builder1.setMessage("Your BMI means: Obese Class I (Moderately obese)"); }
-        else if ( (g.getResultInput() >= 35 ) && (g.getResultInput() < 40)  ) {
-            builder1.setMessage("Your BMI means: Obese Class II (Severely obese)"); }
-        else if ( (g.getResultInput() > 40 ) ) {
-            builder1.setMessage("Your BMI means: Obese Class III (Very severely obese)"); }
+        builder1.setMessage(mes);
 
         builder1.setCancelable(true);
         builder1.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
@@ -151,6 +174,32 @@ public class bmi extends AppCompatActivity {
         AlertDialog alert11 = builder1.create();
         alert11.show();
 
+    }
+
+    // gets the meaning of the bmi
+    public String getMeaning(){
+
+        String message = "";
+
+        if ( g.getResultInput() < 15 ) {
+            message = "Your BMI means: very severely underweight.";}
+        else if ( (g.getResultInput() >= 15 ) && (g.getResultInput() < 16)  ) {
+            message = "Your BMI means: severely underweight."; }
+        else if ( (g.getResultInput() >= 16 ) && (g.getResultInput() < 18.5)  ) {
+            message = "Your BMI means: underweight."; }
+        else if ( (g.getResultInput() >= 18.5 ) && (g.getResultInput() < 25)  ) {
+            message = "Your BMI means: normal."; }
+        else if ( (g.getResultInput() >= 25 ) && (g.getResultInput() < 30)  ) {
+            message = "Your BMI means: overweight."; }
+        else if ( (g.getResultInput() >= 30 ) && (g.getResultInput() < 35)  ) {
+            message = "Your BMI means: Obese Class I (Moderately obese)."; }
+        else if ( (g.getResultInput() >= 35 ) && (g.getResultInput() < 40)  ) {
+            message = ("Your BMI means: Obese Class II (Severely obese)."); }
+        else if ( (g.getResultInput() > 40 ) ) {
+            message = "Your BMI means: Obese Class III (Very severely obese)."; }
+
+        resultMeaning = message;
+        return message;
     }
 
 
